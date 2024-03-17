11ca9526a500b0459f1a8ac2cb714f2728775370
Elhaam <e4elhaam@gmail.com>
2024-03-17 17:27:39 +0500

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
+
+app = FastAPI()
+
+# defining types here 
+class ChoiceBase(BaseModel):
+    choice_text: str
+    is_correct: bool
+
+class QuestionBase(BaseModel):
+    question_text: str
+    choices: List[ChoiceBase]
+
+
---
+++ models.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Boolean, Column, Integer, SavepointClause
+
+
